8f9bdfd81a86462b8e9d2f0425c656e3de1ddccf
Mateus Aguiar <46790429+mateusaguiar@users.noreply.github.com>
2026-01-16 15:58:35 +0000

    fix production rendering

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    host: true
   },
   build: {
     outDir: 'dist',
-  }
+    sourcemap: false
+  },
+  base: '/'
 });
